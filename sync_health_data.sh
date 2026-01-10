4643c13e45443abdb736427f015b1cc3f9cb4619
Joel Gerard <joelgerard@gmail.com>
2026-01-10 12:20:13 -0800

    health synching for corp

--- sync_health_data.sh
+++ sync_health_data.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+trap "exit" INT
+set -x
+
 # Configuration
 DEST_DIR="$HOME/GarminDBSync"
 
 # Source Directories
-SRC1="/Users/joelgerard/Library/CloudStorage/GoogleDrive-joelgerard@gmail.com/My Drive/joel health/HealthData/"
-SRC2="/Users/joelgerard/Library/CloudStorage/GoogleDrive-joelgerard@gmail.com/My Drive/joel health/tree health/tree_home/HealthData/"
+SRC1="/Users/joelgerard/Library/CloudStorage/GoogleDrive-joelgerard@gmail.com/My Drive/joel health/HealthData/DBs/"
+SRC2="/Users/joelgerard/Library/CloudStorage/GoogleDrive-joelgerard@gmail.com/My Drive/joel health/tree health/tree_home/HealthData/DBs/"
 
 # Destination Subdirectories
-DEST1="$DEST_DIR/joel/HealthData/"
-DEST2="$DEST_DIR/tree/HealthData/"
+DEST1="$DEST_DIR/joel/HealthData/DBs/"
+DEST2="$DEST_DIR/tree/HealthData/DBs/"
 
 # Create destination directories if they don't exist
 mkdir -p "$DEST1"
@@ -19,8 +22,8 @@ echo "Starting Sync..."
 
 # Sync Source 1
 if [ -d "$SRC1" ]; then
-    echo "Syncing HealthData..."
-    rsync -avP --delete "$SRC1" "$DEST1"
+    echo "Syncing HealthData DBs..."
+    rsync -avPh --delete --stats "$SRC1" "$DEST1"
 else
     echo "Error: Source 1 directory does not exist: $SRC1"
     exit 1
@@ -28,8 +31,8 @@ fi
 
 # Sync Source 2
 if [ -d "$SRC2" ]; then
-    echo "Syncing tree_home/HealthData..."
-    rsync -avP --delete "$SRC2" "$DEST2"
+    echo "Syncing tree_home/HealthData DBs..."
+    rsync -avPh --delete --stats "$SRC2" "$DEST2"
 else
     echo "Error: Source 2 directory does not exist: $SRC2"
     exit 1
